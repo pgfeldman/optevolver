589f6f5237eeec4ac10522e8e9fc82fff83b5c30
adm-philip.feldman <adm-philip.feldman@Vistronix.com>
2019-12-10 11:27:25 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Evolutionary parameter search for architecture and hyperparameters',
+    author='Phil Feldman',
+    license='MIT',
+)
