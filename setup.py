edef9a2137a97ad518da88a76710c6e68df09d68
Philip.Feldman <philip.feldman@asrcfederal.com>
2019-12-23 09:45:53 -0500

    Initial documentation

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     packages=find_packages(),
     version='0.1.4',
     description='Evolutionary parameter search for architecture and hyperparameters',
+    long_description='Contains packages for creating sequence data (generators), working with Excel and JSON files (util) and evolutionary discovery of architecture and hyperparameters (hyperparameter). It is designed to work with Tensorflow 2.0 and can take advantage of multiple processors',
     author='Phil Feldman',
     license='MIT',
     long_description_content_type="text/markdown",
